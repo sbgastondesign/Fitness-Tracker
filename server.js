e9405e141540cfb3ea8304f42e6b01c7df6e7229
Steph G <61924023+sbgastondesign@users.noreply.github.com>
2021-08-28 22:05:01 -0400

    linked MongoDB Atlas to Heroku

--- server.js
+++ server.js
@@ -10,10 +10,20 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
-    useNewUrlParser: true,
-    useFindAndModify: false
-});
+// mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+//     useNewUrlParser: true,
+//     useFindAndModify: false
+// });
+mongoose.connect(
+    process.env.MONGODB_URI || 'mongodb://localhost/nameless-ocean-85982',
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+        useFindAndModify: false
+    }
+);
+
 
 // routes
 app.use(require("./routes/api.js"));
