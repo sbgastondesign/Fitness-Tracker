35720f7c8e01c1c2a1cbef008a20f876e13f7d67
Steph G <61924023+sbgastondesign@users.noreply.github.com>
2021-08-28 21:06:15 -0400

    updated workout.js with workoutSchema

--- model/workout.js
+++ model/workout.js
@@ -3,18 +3,41 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const workoutSchema = new Schema({
+    type: {
+        type: String,
+        trim: true,
+        required: "Type of Workout"
+    },
     date: {
         type: Date,
         default: Date.now
-    }
-    cardio: {
-        type: String,
-        trim: true,
-        required: "Enter cardio exercise"
     },
-    weight: {
+    totalDuration: {
         type: Number,
-        required: "Enter an amount"
+        trim: true,
+        required: "Total Workout Duration"
+    },
+    numExercises: {
+        type: Number,
+        trim: true,
+        required: "Exercises Performed"
+    },
+    totalWeight: {
+        type: Number,
+        trim: true,
+        required: "Total Weight Lifted"
+    },
+    totalSets: {
+        type: Number,
+        required: "Total Sets Performed"
+    },
+    totalReps: {
+        type: Number,
+        required: "Total Reps Performed"
+    },
+    totalDistance: {
+        type: Number,
+        required: "Total Distance Covered"
     },
 });
 
