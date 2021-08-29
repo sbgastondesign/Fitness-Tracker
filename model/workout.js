cf6dbb41513c3b6c8bd493b402809f4844496bc6
Steph G <61924023+sbgastondesign@users.noreply.github.com>
2021-08-28 21:12:27 -0400

    workout.js schema updated

--- model/workout.js
+++ model/workout.js
@@ -3,42 +3,48 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const workoutSchema = new Schema({
-    type: {
-        type: String,
-        trim: true,
-        required: "Type of Workout"
-    },
     date: {
         type: Date,
         default: Date.now
     },
-    totalDuration: {
-        type: Number,
-        trim: true,
-        required: "Total Workout Duration"
-    },
-    numExercises: {
-        type: Number,
-        trim: true,
-        required: "Exercises Performed"
-    },
-    totalWeight: {
-        type: Number,
-        trim: true,
-        required: "Total Weight Lifted"
-    },
-    totalSets: {
-        type: Number,
-        required: "Total Sets Performed"
-    },
-    totalReps: {
-        type: Number,
-        required: "Total Reps Performed"
-    },
-    totalDistance: {
-        type: Number,
-        required: "Total Distance Covered"
-    },
+    exercises: [
+        {
+            type: {
+                type: String,
+                trim: true,
+                required: "Type of Workout"
+            },
+            exerciseName: {
+                type: String,
+                trim: true,
+                required: "Name of Workout"
+            },
+            totalDuration: {
+                type: Number,
+                required: "Total Workout Duration"
+            },
+            numExercises: {
+                type: Number,
+                required: "Exercises Performed"
+            },
+            totalWeight: {
+                type: Number,
+                required: "Total Weight Lifted"
+            },
+            totalSets: {
+                type: Number,
+                required: "Total Sets Performed"
+            },
+            totalReps: {
+                type: Number,
+                required: "Total Reps Performed"
+            },
+            totalDistance: {
+                type: Number,
+                required: "Total Distance Covered"
+            },
+        },
+    ]
 });
 
 const workoutSchema = mongoose.model("workout", workoutSchema);
